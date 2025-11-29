PaperMC/Paper

Better Stats API

paper-server/src/main/java/io/papermc/paper/registry/PaperRegistries.java
@@ -11,6 +11,10 @@ import io.papermc.paper.registry.data.PaperGameEventRegistryEntry;
 import io.papermc.paper.registry.data.PaperPaintingVariantRegistryEntry;
 import io.papermc.paper.registry.entry.RegistryEntry;
 import io.papermc.paper.registry.tag.TagKey;
+import io.papermc.paper.statistic.CustomStatistic;
+import io.papermc.paper.statistic.PaperCustomStatistic;
+import io.papermc.paper.statistic.PaperStatisticType;
+import io.papermc.paper.statistic.StatisticType;
 import java.util.Collections;
 import java.util.IdentityHashMap;
 import java.util.List;
@@ -98,6 +102,9 @@ public final class PaperRegistries {
             start(Registries.FLUID, RegistryKey.FLUID).craft(Fluid.class, CraftFluid::new).build(),
             start(Registries.SOUND_EVENT, RegistryKey.SOUND_EVENT).craft(Sound.class, CraftSound::new).build(),
             start(Registries.DATA_COMPONENT_TYPE, RegistryKey.DATA_COMPONENT_TYPE).craft(DataComponentTypes.class, PaperDataComponentType::of).build(),
+            start(Registries.CUSTOM_STAT, RegistryKey.CUSTOM_STAT).craft(CustomStatistic.class, PaperCustomStatistic::new).build(),
+            start(Registries.STAT_TYPE, RegistryKey.STAT_TYPE).craft(StatisticType.class, PaperStatisticType::create).build(),
+
 
             // data-drivens
             start(Registries.BIOME, RegistryKey.BIOME).craft(Biome.class, CraftBiome::new).build().delayed(),

Pulse-MC/Pulse-Paper

paper-server/src/main/java/io/papermc/paper/registry/PaperRegistries.java
@@ -25,6 +25,10 @@ import io.papermc.paper.registry.data.dialog.PaperDialogRegistryEntry;
 import io.papermc.paper.registry.entry.RegistryEntry;
 import io.papermc.paper.registry.entry.RegistryEntryMeta;
 import io.papermc.paper.registry.tag.TagKey;
+import io.papermc.paper.statistic.CustomStatistics;
+import io.papermc.paper.statistic.PaperCustomStatistic;
+import io.papermc.paper.statistic.PaperStatisticType;
+import io.papermc.paper.statistic.StatisticTypes;
 import java.util.Collections;
 import java.util.IdentityHashMap;
 import java.util.List;
@@ -121,6 +125,8 @@ public final class PaperRegistries {
             start(Registries.SOUND_EVENT, RegistryKey.SOUND_EVENT).craft(Sound.class, CraftSound::new, true).create(PaperSoundEventRegistryEntry.PaperBuilder::new, RegistryEntryMeta.RegistryModificationApiSupport.NONE),
             start(Registries.DATA_COMPONENT_TYPE, RegistryKey.DATA_COMPONENT_TYPE).craft(DataComponentTypes.class, PaperDataComponentType::of).build(),
             start(Registries.GAME_RULE, RegistryKey.GAME_RULE).craft(GameRule.class, CraftGameRule::new).build(),
+            start(Registries.CUSTOM_STAT, RegistryKey.CUSTOM_STAT).craft(CustomStatistics.class, PaperCustomStatistic::new).build(),
+            start(Registries.STAT_TYPE, RegistryKey.STAT_TYPE).craft(StatisticTypes.class, PaperStatisticType::create).build(),
 
             // data-driven
             start(Registries.BIOME, RegistryKey.BIOME).craft(Biome.class, CraftBiome::new).build().delayed(),